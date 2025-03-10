6742d9106407ab1b54274b646e4144a29b9007c6
akshat01-ar <akshatgautamg1@gmail.com>
2025-03-10 16:07:03 +0530

    Add files via upload

---
+++ upi_qr.py
@@ -0,0 +1,8 @@
+import qrcode as qr
+print("only valid for upi_id not for mob number ")
+upi = input("enter your upi id : ")
+js = f"upi://pay?pa={upi}&pn=Recipient%20Name&mc=1234"
+img = qr.make(js)
+img.save(f"QR_{upi}.png")
+
+    
